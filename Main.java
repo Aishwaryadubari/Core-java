e67ad74d835688d656ffed898cfdf44c9948a67b
Aishwarya D <60175634+Aishwaryadubari@users.noreply.github.com>
2022-08-12 18:11:05 +0530

    inheritance

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
-	public static void main(String[] args) {
-	    Animal animal = new Animal("Animal", 1, 1, 5, 5);
-
-        Dig dog = new Dig("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
-
-        dog.walk();
+	 public static void main(String[] args) {
+	Outlander outlander = new Outlander(36);
+    outlander.steer(45);
+    outlander.accelerate(30);
+    outlander.accelerate(20);
+    outlander.accelerate(-42);
 
 }
 }
